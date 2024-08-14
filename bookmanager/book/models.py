711989bd5d2e9daaeda3290d04a3a99d03fd0535
myproject <linjc040204@outlook.com>
2024-08-14 22:07:26 +0800

    定义orm模型

--- bookmanager/book/models.py
+++ bookmanager/book/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class BookInfo(models.Model):
+    #id
+    name = models.CharField(max_length=10)
+    pass
+class PeopleInfo(models.Model):
+    name = models.CharField(max_length=10)
+    gender = models.BooleanField()
+
+    book = models.ForeignKey(BookInfo,on_delete=models.CASCADE)
